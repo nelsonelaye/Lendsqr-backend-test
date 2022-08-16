4f41dd88d6b8d5284f13a005fa5d1b14438da3a8
nelsonelaye <nelsonelaye@gmail.com>
2022-08-16 15:23:07 +0100

    database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-
-const url = "mongodb://localhost/lendsqrWallet";
+require("dotenv").config();
+// const url = "mongodb://localhost/lendsqrWallet";
+const url = process.env.ATLAS;
 
 mongoose
   .connect(url)
